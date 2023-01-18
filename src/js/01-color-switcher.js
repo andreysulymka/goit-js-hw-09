349c4b40d406eebc140e4f5986618b5560f1ec0e
andreysulymka <andreysulymka5@gmail.com>
2023-01-18 16:29:32 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,14 +3,14 @@ const stopKey = document.querySelector('button[data-stop]');
 
 let timeoutId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
 startKey.addEventListener('click', onClick);
 
 function onClick() {
     timeoutId = setInterval(getRandomHexColor, 1000);
-};
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
 stopKey.addEventListener('click', onStop);

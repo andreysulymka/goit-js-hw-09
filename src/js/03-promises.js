577833773d2db3a5f1220d26fdd84882a01aa4ec
andreysulymka <andreysulymka5@gmail.com>
2023-01-23 15:48:03 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onSubmit);
+
+const firstdelayRef = document.getElementsByName('delay');
+const stepRef = document.getElementsByName('step');
+const amountRef = document.getElementsByName('amount');
+
+firstdelayRef.addEventListener('input', (event) => {
+  const furstDelay = event.currentTarget.value;
+});
+stepRef.addEventListener('input', (event) => {
+  const step = event.currentTarget.value;
+});
+amountRef.addEventListener('input', (event) => {
+  const amount = event.currentTarget.value;
+});
+
+const promises = [];
+for (i = 0; i < amount, i++) { };
+
+function onSubmit() {
+  
+};
+function start(amount, createPromise) {
+  
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(
+        setTimeout(() => { resolve({ position, delay }); }, delay)
+      );
+    } else {
+      reject(  setTimeout(() => { reject({ position, delay }); }, delay));
+    }
+  });
+}
+
